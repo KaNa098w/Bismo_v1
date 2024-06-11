7e5dcdb5b73b04ad1c79450d2789be3a5d70aa2c
noname110698 <noname110698@gmail.com>
2024-06-11 22:08:29 +0500

    123

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bismo',
+      title: 'Bismo1',
       theme: AppTheme().lightTheme,
       darkTheme: AppTheme().darkTheme,
       themeMode: ThemeMode.light,
